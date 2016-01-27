91be2db956d811e409ab14feda7bf824975b6f93
divyacoder <divya.sachan@buyhatke.com>
2016-01-27 15:43:08 +0530

    First commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+$('.borderText').on('click',function(){
+   $('.borderText').removeClass('active');
+    $(this).addClass('active');
+});
+$("#blueText1").on('click', function(){
+	
+	$("#list1").removeClass('noDisplay');
+	$("#blueText1").hide();
+});
+$("#blueText2").on('click', function(){
+	
+	$("#list1").addClass('noDisplay');
+	$("#blueText2").hide();
+	$("#blueText1").show();
+});
---
+++ js/homeData.js
@@ -0,0 +1,3 @@
+$.get("http://localhost:8080/carafe/api/v1/product", function(data, status){
+        console.log("Data: " + data + "\nStatus: " + status);
+    });
